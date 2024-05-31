6adc99c21cde99f15ce693941f231306bb27d009
Dilshad <a-rustacean@outlook.com>
2024-05-31 11:42:13 +0530

    chore: update

--- .github/SECURITY.md
+++
@@ -1,7 +0,0 @@
-# Security Policy
-
-## Supported Versions
-
-## Reporting a Vulnerability
-
-If you find any potential vulnerability, please send an e-mail to [Dilshad](mailto:a-rustacean@outlook.com). 
--- crates/oxc_parser/src/state.rs
+++
@@ -1,9 +0,0 @@
-use oxc_ast::ast::Decorator;
-use rustc_hash::FxHashSet;
-
-#[derive(Default)]
-pub struct ParserState<'a> {
-    pub not_parenthesized_arrow: FxHashSet<u32>,
-
-    pub decorators: Vec<Decorator<'a>>,
-}
